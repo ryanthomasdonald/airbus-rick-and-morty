85c2cdcf8081dc7b87101302dc9cd40be4ad4652
Ryan Donald <ryanthomasdonald@gmail.com>
2023-05-25 02:19:55 -0400

    button styling done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import TopLogo from "./components/TopLogo.js"
+import NavButtons from "./components/NavButtons.js"
 import './App.css';
 
 function App() {
+
+    const [cards, setCards] = useState([]);
+    const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character?page=1')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCards(data.results);
+                setInfo(data.info.pages);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
     return (
-        <div className="row justify-content-center g-0">
-            <img src="assets/rick-and-morty-api-logo.png" className="topLogo" alt="rick and morty logo"/>
+        <div>
+            <TopLogo />
+            <NavButtons />
+            <div>
+                {info}
+            </div>
+            <div>
+                {cards.map((card) => {
+                    return (
+                        <div key={card.id}>
+                            {card.name}
+                            <img src={card.image} alt="avatar" />
+                        </div>
+                    );
+                })};
+            </div>
+            {/* <div className="card">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#!" className="btn btn-primary">Button</a>
+                </div>
+            </div> */}
         </div>
     );
 };
---
+++ src/components/TopLogo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TopLogo.css";
+
+function TopLogo() {
+    return(
+        <div className="row justify-content-center g-0">
+            <img src="assets/rick-and-morty-api-logo.png" className="topLogo" alt="rick and morty logo"/>
+        </div>
+    );
+};
+
+export default TopLogo;
